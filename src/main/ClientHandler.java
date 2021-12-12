9371f3910fb435bd5b51390ce43659e2d66ef67b
DESKTOP-GT3I9SR\Munja <djordjijeradovic24@gmail.com>
2021-12-12 23:40:22 +0100

    Added isVaccined(int i) method to ClientHandler class; Finished registration

--- src/main/ClientHandler.java
+++ src/main/ClientHandler.java
@@ -30,6 +30,9 @@ public class ClientHandler extends Thread {
 	String jmbg;
 	String gender;
 	String email;
+	String vaccine1 = null;
+	String vaccine2 = null;
+	String vaccine3 = null;
 	
 	boolean isQuit=false;
 	
@@ -51,6 +54,108 @@ public class ClientHandler extends Thread {
 			clientOutput.println("Greska prilikom konekcija servera i klijenta.");
 		} 
 		
+	}
+	
+	public boolean isVaccined(int i) throws IOException {
+		String response;
+		while(true) {
+			clientOutput.println(">>> Da li ste primili " + i + ". vakcinu? (da/ne)");
+			response = clientInput.readLine();
+			if(response.toLowerCase().equals("da")) {
+				
+				while(true) {
+					clientOutput.println(">>> Koju ste vakcinu primili? Ukucajte odgovarajuci broj.\n1.Fajzer\n2.Sputnjik\n3.Sinofarm\n4.Oksford/AstraZeneka");
+					switch (i) {
+					case 1:
+						response = clientInput.readLine();
+						switch(response) {
+						case "1":
+							vaccine1 = "Fajzer";
+							break;
+						case "2":
+							vaccine1 = "Sputnjik";
+							break;
+						case "3":
+							vaccine1 = "Sinofarm";
+							break;
+						case "4":
+							vaccine1 = "Oksford/AstraZeneka";
+							break;
+						default:
+							clientOutput.println("Niste ispravno uneli odgovarajuci broj.");
+							continue;
+						}
+						return true;
+					
+					case 2:
+						response = clientInput.readLine();
+						switch(response) {
+						case "1":
+							if(!vaccine1.equals("Fajzer")) {
+								clientOutput.println("Niste uneli ispravno podatak za drugu primljenu vakcinu. Mora biti od istog proizvodjaca kao i prva primljena vakcina.");
+								return false;
+							}
+							vaccine2 = "Fajzer";
+							break;
+						case "2":
+							if(!vaccine1.equals("Sputnjik")) {
+								clientOutput.println("Niste uneli ispravno podatak za drugu primljenu vakcinu. Mora biti od istog proizvodjaca kao i prva primljena vakcina.");
+								return false;
+							}
+							vaccine2 = "Sputnjik";
+							break;
+						case "3":
+							if(!vaccine1.equals("Sinofarm")) {
+								clientOutput.println("Niste uneli ispravno podatak za drugu primljenu vakcinu. Mora biti od istog proizvodjaca kao i prva primljena vakcina.");
+								return false;
+							}
+							vaccine2 = "Sinofarm";
+							break;
+						case "4":
+							if(!vaccine1.equals("Oksford/AstraZeneka")) {
+								clientOutput.println("Niste uneli ispravno podatak za drugu primljenu vakcinu. Mora biti od istog proizvodjaca kao i prva primljena vakcina.");
+								return false;
+							}
+							vaccine2 = "Oksford/AstraZeneka";
+							break;
+						default:
+							clientOutput.println("Niste ispravno uneli odgovarajuci broj.");
+							continue;
+						}
+						return true;
+						
+					case 3:
+						response = clientInput.readLine();
+						switch(response) {
+						case "1":
+							vaccine3 = "Fajzer";
+							break;
+						case "2":
+							vaccine3 = "Sputnjik";
+							break;
+						case "3":
+							vaccine3 = "Sinofarm";
+							break;
+						case "4":
+							vaccine3 = "Oksford/AstraZeneka";
+							break;
+						default:
+							clientOutput.println("Niste ispravno uneli odgovarajuci broj.");
+							continue;
+						}
+						return true;
+						
+					default:
+						break;
+					}
+				}
+			} else if(response.toLowerCase().equals("ne")) {
+				return false;
+			} else {
+				clientOutput.println(">>> Niste uspesno odgovorili na postavljeno pitanje.\n>>> Ukoliko budete zeleli da prekinete sesiju, upisite ***quit");
+				continue;
+			}
+		}
 	}
 	
 	public boolean registerClient() {
@@ -163,8 +268,7 @@ public class ClientHandler extends Thread {
 					isQuit = true;
 					return false;
 				}
-				
-				if(!gender.equals("musko") && !gender.equals("zensko")) {
+				if(!gender.toLowerCase().equals("musko") && !gender.toLowerCase().equals("zensko")) {
 					gender=null;
 					clientOutput.println(">>> Niste uspesno uneli pol.\n>>> Ukoliko budete zeleli da prekinete sesiju, upisite ***quit");
 					continue;
@@ -188,13 +292,33 @@ public class ClientHandler extends Thread {
 				break;
 			}
 			
+			for(int i = 1; i <= 3; i++) {
+				if(isVaccined(i)==false) {
+					break;
+				}
+			}
+			
+			String query = "insert into appuser values(?,?,?,?,?,?,?,?,?,?)";
+		    preparedStatement = connect.prepareStatement(query);
+		    preparedStatement.setString(1, username);
+		    preparedStatement.setString(2, password);
+		    preparedStatement.setString(3, name);
+		    preparedStatement.setString(4, surname);
+		    preparedStatement.setString(5, jmbg);
+		    preparedStatement.setString(6, gender);
+		    preparedStatement.setString(7, email);
+		    preparedStatement.setString(8, vaccine1);
+		    preparedStatement.setString(9, vaccine2);
+		    preparedStatement.setString(10, vaccine3);
+		    preparedStatement.executeUpdate();
+		    
 			return true;
 			
 		} catch (IOException e) {
 			clientOutput.println("Greska pri registraciji.");
 			return false;
 		} catch (SQLException e) {
-			e.printStackTrace();
+			//e.printStackTrace();
 			clientOutput.println("Greska tokom registracija prilikom povezivanja sa bazom podataka.");
 			System.out.println("Greska tokom registracija prilikom povezivanja sa bazom podataka.");
 			return false;
@@ -218,40 +342,32 @@ public class ClientHandler extends Thread {
 	public void run() {
 		
 		try {
-			
 			String response;
-			
 			while(true) {
-				
-				if(connect == null) {
+				if(connect == null) {	// ako nije uspesno uspostavljena konekcija sa bazom podataka (koja se uspostavlja u konstruktoru)
 					break;
 				}
-				
 				clientOutput.println(">>> Ukoliko budete zeleli da prekinete sesiju, upisite ***quit\n Upisite broj (1 ili 2) za zeljenu opciju:\n1.Login\n2.Registracija");
-				
 				response = clientInput.readLine();
-				
 				if(response.equals("***quit")) {
 					break;
 				} else if(response.equals("1")) {
+					//LOGIN
+					//clientOutput.println(">>> Unesite korisnicko ime: ");
 					
-					clientOutput.println(">>> IZABRALI STE PRVU OPCIJU");
+					//clientOutput.println(">>> Unesite sifru: ");
+					
+					
 					
 				} else if(response.equals("2")){
-			
+					//REGISTRACIJA
 						boolean isRegistered = registerClient();
-					
 						if(isQuit == true) {
 							break;
 						}
-					
 						if(isRegistered == false) {
 							break;
 						}
-					
-					//System.out.println(username +  " " + password + " " + name + " " + surname + " " + jmbg + " " + gender + " " + email);	
-					//clientOutput.println(">>> IZABRALI STE DRUGU OPCIJU");
-					
 				} else {
 					clientOutput.println(">>> Niste uneli ispravno.");
 					continue;
@@ -259,6 +375,7 @@ public class ClientHandler extends Thread {
 				
 			}
 				
+			
 			clientOutput.println(">>> Dovidjenja.");
 			
 			//Server.onlineUsers.remove(this);
